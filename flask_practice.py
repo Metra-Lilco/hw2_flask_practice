0703add532fdc136be8c6c2a07149bc77e2589e8
Lilco <metra.lilco@gmail.com>
2023-09-17 15:23:46 +0300

    Add 'read_requirements' function

--- flask_practice.py
+++ flask_practice.py
@@ -12,5 +12,13 @@ def index():
     return "Index Page"
 
 
+@app.route("/requirements")
+def read_requirements():
+    with open("requirements.txt", "r", encoding="utf-8") as file:
+        content = file.readlines()
+    content_str = "<br>".join(content)
+    return content_str
+
+
 if __name__ == "__main__":
     app.run()
